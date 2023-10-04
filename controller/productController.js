f487497dfee59d8cc33dc7623a6318e2658d895c
maulanaabdullana2 <maulanaabdullana123@gmail.com>
2023-10-04 22:51:33 +0700

    Crud SQL

--- controller/productController.js
+++ controller/productController.js
@@ -23,6 +23,77 @@ const createProduct = async (req, res) => {
   }
 };
 
+const findProducts = async (req, res) => {
+  try {
+    const products = await Product.findAll();
+    res.status(200).json({
+      status: "success",
+      data: {
+        products,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      status: "failed",
+      message: error.message,
+    });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  const { name, price, stock } = req.body;
+  try {
+    const product = await Product.update(
+      {
+        name,
+        stock,
+        price,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    req.status(400).json({
+      status: "failed",
+      mesagge: err.mesagge,
+    });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        product,
+      },
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: "failed",
+      mesagge: err.mesagge,
+    });
+  }
+};
+
 module.exports = {
   createProduct,
+  findProducts,
+  updateProduct,
+  deleteProduct,
 };
--- routes/productRouter.js
+++ routes/productRouter.js
@@ -2,5 +2,8 @@ const router = require("express").Router();
 const Product = require("../controller/productController");
 
 router.post("/", Product.createProduct);
+router.get("/", Product.findProducts);
+router.patch("/:id", Product.updateProduct);
+router.delete("/:id", Product.deleteProduct);
 
 module.exports = router;
